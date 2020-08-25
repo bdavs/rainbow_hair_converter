a0f1d44bac5f5abf91d601f9b9bd979ba4882aee
Robert Davis <rdavis4677@gmail.com>
2020-08-25 10:56:11 -0400

    rework rainbow method, add config file thing

---
+++ default_parameters.py
@@ -0,0 +1,7 @@
+min_island_size = 400
+num_colors = 6
+saturation = 255
+valueC = 255
+hrange = 20
+srange = 60
+vrange = 50
--- make_gif.py
+++ make_gif.py
@@ -1,9 +1,18 @@
 import animeface
 import requests
+import math
 from PIL import Image
 import cv2
 import numpy as np
+import default_parameters 
 
+def bound(minx, x, maxx):
+    if x < minx: return minx
+    if x > maxx: return maxx
+    return x
+
+def remap(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def color_object_to_list(color):
     # rgb = []
@@ -49,6 +58,36 @@ def clustering(img):
 
     return (res2)
 
+
+def color_mask(color,img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # hsv = cv2.cvtColor(cvim, cv2.COLOR_BGR2HSV)
+
+    #grab hair color
+    color2 = color_object_to_list(color)
+    # print(color2) #84,80,83
+    haircolor =   cv2.cvtColor(np.uint8([[color2]]),cv2.COLOR_BGR2HSV)
+    # print(haircolor[0,0]) #143,12,84
+
+    #hair hsv thresholds
+    hrange = default_parameters.hrange
+    srange = default_parameters.srange
+    vrange = default_parameters.vrange
+    lower_color = np.array([bound(0,haircolor[0,0,0]-hrange,180),
+                            bound(0,haircolor[0,0,1]-srange,255),
+                            bound(0,haircolor[0,0,2]-vrange,255)
+                            ])
+    # lower_red = np.array([30,150,50])
+    upper_color = np.array([bound(0,haircolor[0,0,0]+hrange,180),
+                            bound(0,haircolor[0,0,1]+srange,255),
+                            bound(0,haircolor[0,0,2]+vrange,255)
+                            ])
+    # upper_red = np.array([255,255,180])
+
+    # get everything between the thresholds
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    return(mask)
+
 def noise_reduction(img):
     #noise reduction
     # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
@@ -61,41 +100,35 @@ def noise_reduction(img):
 
     return(opened)
 
-def color_mask(color,img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # hsv = cv2.cvtColor(cvim, cv2.COLOR_BGR2HSV)
-
-    #grab hair color
-    color2 = color_object_to_list(color)
-    # print(color2) #84,80,83
-    haircolor = cv2.cvtColor(np.uint8([[color2]]),cv2.COLOR_BGR2HSV)
-    # print(haircolor[0,0]) #143,12,84
-
-    #hair hsv thresholds
-    lower_color = np.array([haircolor[0,0,0]-25,0,50])
-    # lower_red = np.array([30,150,50])
-    upper_color = np.array([haircolor[0,0,0]+25,155,205])
-    # upper_red = np.array([255,255,180])
-
-    # get everything between the thresholds
-    mask = cv2.inRange(hsv, lower_color, upper_color)
-    return(mask)
-
-def contouring(original,mask):
+def contouring(original,mask,min_island_size):
     mask_new = np.zeros(mask.shape,np.uint8)
     contours, hier = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        if 180<cv2.contourArea(cnt):
+        if min_island_size<cv2.contourArea(cnt):
             cv2.drawContours(original,[cnt],0,(0,255,0),2)
             cv2.drawContours(mask_new,[cnt],0,255,-1)
 
     return(original,mask_new)
 
-def image_to_rainbow_gif(image,binarymask,gif_file):
+def image_to_rainbow_gif(image,binarymask,num_colors,gif_file):
     # take single image and mask and output rainbow gif
     image_array = []
     # quick lut to change colors
-    rgb_lut = [(0,0,255),(0,255,255),(0,255,0),(255,255,0),(255,0,0),(255,0,255)]
+    # rgb_lut = [(0,0,255),(0,255,255),(0,255,0),(255,255,0),(255,0,0),(255,0,255)]
+    rgb_lut = []
+    h=0
+    s=default_parameters.saturation
+    v=default_parameters.valueC
+    # num_colors=10
+    width = 180/num_colors
+    for i in range(num_colors):
+        h += width
+        rgb_lut.append(cv2.cvtColor(np.uint8([[[h,s,v]]]),cv2.COLOR_HSV2RGB)[0,0].tolist())
+
+
+    
+
+    # print(rgb_lut)
     # loop through all combinations of colors
     for rgb_val in rgb_lut:
 
@@ -129,36 +162,47 @@ def main(url=None, single_file=None, output_folder=None):
     original = cvim.copy()
     # img = cvim.copy()
 
+    #get facial feautures, namely hair color
     faces = anime_face_features(image_file)
 
-    # cluster the colors
-    clustered = clustering(cvim)
 
+    clusterLevel = 0
     #create a mask from the colors
-    mask = color_mask(faces[0].hair.color, clustered)
+    if clusterLevel > 0:
+        # cluster the colors
+        clustered = clustering(cvim)
+        cv2.imwrite('alt_methods/clustered.png',clustered)
+        mask = color_mask(faces[0].hair.color, clustered)
+    else:
+        mask = color_mask(faces[0].hair.color, cvim)
+
+    # mask = cv2.medianBlur(mask,3)
 
     #reduce the noise in the image
     mask = noise_reduction(mask)
 
-    
-    original,mask = contouring(original,mask)
+    #trace around the largest areas
+    original,mask = contouring(original,mask,default_parameters.min_island_size)
 
+    mask = cv2.GaussianBlur(mask,(11,11),10)
 
     #show non masked area
     res = cv2.bitwise_and(cvim,cvim, mask= mask)
 
-    #write everything to files
-    cv2.imwrite('cvim.png',cvim)
-    cv2.imwrite('mask.png',mask)
-    cv2.imwrite('res.png',res)
-    cv2.imwrite('contoured.png',original)
-    cv2.imwrite('clustered.png',clustered)
+    #write everything to files for development
+    cv2.imwrite('alt_methods/cvim.png',cvim)
+    cv2.imwrite('alt_methods/mask.png',mask)
+    cv2.imwrite('alt_methods/res.png',res)
+    cv2.imwrite('alt_methods/contoured.png',original)
 
+    # convert to rgb
     cvim_rgb = cv2.cvtColor(cvim, cv2.COLOR_BGR2RGB)
-    image_to_rainbow_gif(cvim_rgb,mask,output)
+
+    #execute rainbow function
+    image_to_rainbow_gif(cvim_rgb,mask,default_parameters.num_colors,output)
 
 if __name__ == '__main__':
-    filename = '/files/Documents/GitHub/face-seg/data/downloads/input.png'
+    filename = '/project/data/downloads/input.png'
     main(single_file=filename,output_folder=".")
 
 # rgb_val = faces[0].hair.color
